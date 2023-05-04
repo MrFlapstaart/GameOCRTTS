e4dcdab4ea46723334013bfcebd3699e6ba37600
Sven Serlier <85389871+wrt54g@users.noreply.github.com>
2023-05-04 13:17:19 +0200

    Remove unnecessary usings.

--- AboutForm.cs
+++ AboutForm.cs
@@ -1,13 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GameOCRTTS
